006a819a563048e55105fba2da9fa680b4910552
Kirill Buslovskii <k.buslovsky@yandex.ru>
2025-11-16 23:42:56 +0300

    Realise Runner class

--- src/main/java/com/javarush/island/buslovskii/Runner.java
+++ src/main/java/com/javarush/island/buslovskii/Runner.java
@@ -1,8 +1,18 @@
 package com.javarush.island.buslovskii;
 
+import com.javarush.island.buslovskii.controller.Simulator;
 
 public class Runner {
+    private static final int ISLAND_WIDTH = 100;
+    private static final int ISLAND_HEIGHT = 20;
+
     public static void main(String[] args) {
-        System.out.println("start");
+        try {
+            Simulator simulator = new Simulator(ISLAND_WIDTH, ISLAND_HEIGHT);
+            simulator.startSimulation();
+        } catch (Exception e) {
+            System.err.println("Ошибка: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
